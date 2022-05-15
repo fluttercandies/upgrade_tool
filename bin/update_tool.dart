2896dac910a5b917c3966a590921567972554ce5
nayuta <dujunda@bytedance.com>
2022-05-15 21:25:09 +0800

    add more test

--- bin/update_tool.dart
+++ bin/update_tool.dart
@@ -1,14 +1,16 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> arguments) async {
   Directory currentProject = Directory.current;
 
   String libPath = currentProject.path + '/lib';
-
-  print('Hello world!');
+  handlePath(libPath);
 }
 
 void handlePath(String path) async {
+  print(path);
+
   /// 如果是文件，则进行处理
   if (await FileSystemEntity.isFile(path)) handleCodeFile(path);
 
@@ -22,13 +24,19 @@ void handlePath(String path) async {
       }
     }
   }
-  return;
   // 如果是文件夹，递归处理所有的子集目录
 }
 
 void handleCodeFile(String path) async {
-  
-
-
-
+  if (path.contains('.dart')) {
+    File file = File(path);
+    List<String> codes = await file.readAsLines();
+    for (int i = 0; i < codes.length; i++) {
+      // codes[i].replaceAllMapped("", (match) => "instance");
+      print(codes[i].allMatches(match.toString()));
+    }
+  }
 }
+
+String match =
+    'SchedulerBinding.instance\!|WidgetsBinding.instance\!|GestureBinding.instance\!|RendererBinding.instance\!|ServicesBinding.instance\!| PaintingBinding.instance\!|SemanticsBinding.instance\!';
--- example/lib/test.dart
+++ example/lib/test.dart
@@ -2,10 +2,14 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 
 void test() {
   SchedulerBinding.instance!.runtimeType;
   WidgetsBinding.instance!.runtimeType;
   GestureBinding.instance!.runtimeType;
   RendererBinding.instance!.runtimeType;
+  ServicesBinding.instance!.runtimeType;
+  PaintingBinding.instance!.runtimeType;
+  SemanticsBinding.instance!.runtimeType;
 }
